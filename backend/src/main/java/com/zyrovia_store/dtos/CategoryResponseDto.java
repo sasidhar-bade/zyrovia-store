4b5d937554810abe487c15a70bb32b25ea56cdfb
Sasidhar Bade <sasidharbade@gmail.com>
2026-06-12 15:23:49 +0530

    implement category service layer and DTOs

---
+++ backend/src/main/java/com/zyrovia_store/dtos/CategoryResponseDto.java
@@ -0,0 +1,19 @@
+package com.zyrovia_store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryResponseDto {
+
+	private Long id;
+
+	private String categoryName;
+}
